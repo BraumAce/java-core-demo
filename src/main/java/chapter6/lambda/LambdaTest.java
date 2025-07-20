67699b3a350954c5d09d0c41349c7b15168cf899
BraumAce <siyuan.peng@foxmail.com>
2025-07-20 17:03:47 +0800

    feat(chapter6.lambda): 添加 LambdaTest 类以演示 lambda 表达式和方法引用
    - 新增 LambdaTest 类，包含 main 方法和其他辅助方法
    - 展示数组排序、计时器和重复执行操作的示例
    - 介绍 lambda 表达式在数组排序和事件处理中的应用

---
+++ src/main/java/chapter6/lambda/LambdaTest.java
@@ -0,0 +1,36 @@
+package chapter6.lambda;
+
+import javax.swing.*;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.function.IntConsumer;
+
+public class LambdaTest {
+
+    public static void main(String[] args) {
+        String[] strings = { "Mary", "Sue", "Kate", "Jane", "Jill", "Mike", "Jim", "Joe" };
+        System.out.println(Arrays.toString(strings));
+
+        System.out.println("Sorted in dictionary order: ");
+        Arrays.sort(strings);
+        System.out.println(Arrays.toString(strings));
+
+        System.out.println("Sorted by length: ");
+        Arrays.sort(strings, (a, b) -> a.length() - b.length());
+        System.out.println(Arrays.toString(strings));
+
+        repeat(10, i -> System.out.println("Countdown: " + (9 - i)));
+
+        Timer timer = new Timer(1000, event -> System.out.println("The time is " + new Date()));
+        timer.start();
+
+        JOptionPane.showMessageDialog(null, "Quit program?");
+        System.exit(0);
+    }
+
+    public static void repeat(int n, IntConsumer action) {
+        for (int i = 0; i < n; i++) {
+            action.accept(i);
+        }
+    }
+}
