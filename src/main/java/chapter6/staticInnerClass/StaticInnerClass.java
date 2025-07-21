81a72f4240d26e70a109692aa5bc44d4eb9f973b
BraumAce <siyuan.peng@foxmail.com>
2025-07-21 23:53:41 +0800

    feat(chapter6): 添加静态内部类示例代码
    - 新增 StaticInnerClass 类演示静态内部类的使用
    - 实现 ArrayAlg 类包含静态内部类 Pair 和 minmax 方法
    - 主方法中创建 Pair 对象并输出最小值和最大值

---
+++ src/main/java/chapter6/staticInnerClass/StaticInnerClass.java
@@ -0,0 +1,50 @@
+package chapter6.staticInnerClass;
+
+import lombok.Getter;
+
+/**
+ * 6.3.7 静态内部类
+ * 6-9 静态内部类
+ */
+public class StaticInnerClass {
+    public static void main(String[] args) {
+        double[] values = new double[20];
+        for (int i = 0; i < values.length; i++) {
+            values[i] = 100 * Math.random();
+        }
+
+        ArrayAlg.Pair minmax = ArrayAlg.minmax(values);
+        System.out.println("min = " + minmax.getFirst());
+        System.out.println("max = " + minmax.getSecond());
+    }
+}
+
+class ArrayAlg {
+    @Getter
+    public static class Pair {
+        private final double first;
+        private final double second;
+
+        public Pair(double f, double s) {
+            first = f;
+            second = s;
+        }
+
+    }
+
+    public static Pair minmax(double[] values) {
+        double min = Double.POSITIVE_INFINITY;
+        double max = Double.NEGATIVE_INFINITY;
+
+        for (double v : values) {
+            if (min > v) {
+                min = v;
+            }
+            if (max < v) {
+                max = v;
+            }
+        }
+
+        return new Pair(min, max);
+    }
+}
